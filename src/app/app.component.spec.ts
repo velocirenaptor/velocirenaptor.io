d1b0af623c3da3150059b0ff27382346d9a089b4
Renata Mota <159165553+velocirenaptor@users.noreply.github.com>
2024-09-24 10:09:06 -0600

    app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'mycv' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mycv');
+    expect(app.title).toEqual('Renata Mota');
   });
 
   it('should render title', () => {
